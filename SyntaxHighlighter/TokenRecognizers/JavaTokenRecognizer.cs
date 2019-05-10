4cf8530850b9baee062d171cae7e719770bff5b4
Tomáš Kubín <Kubin.Tom@email.cz>
2019-05-10 09:31:47 +0200

    Fixing whitespaces between java keywords

--- SyntaxHighlighter/TokenRecognizers/JavaTokenRecognizer.cs
+++ SyntaxHighlighter/TokenRecognizers/JavaTokenRecognizer.cs
@@ -8,7 +8,7 @@ namespace SyntaxHighlighter.TokenRecognizers
 {
     public class JavaTokenRecognizer : AbstractTokenRecognizer
     {
-        public static readonly string[] JavaKeywords = "abstract continue for new switch assert default goto package synchronized boolean do if private this break double implements protected throw byte else import public	throws case enum instanceof return transient catch extends int short	try char final interface static void class finally long strictfp volatile const float native super while".Split(' ');
+        public static readonly string[] JavaKeywords = "abstract continue for new switch assert default goto package synchronized boolean do if private this break double implements protected throw byte else import public throws case enum instanceof return transient catch extends int short try char final interface static void class finally long strictfp volatile const float native super while".Split(' ');
         public static readonly string[] JavaSeparators = "( ) { } ; : ,".Split(' ');
         public static readonly string[] JavaOperators = "() [] . ?. ?[] :: + - * & && = == += -= *= &= -> < >  ?? >= <= &lt; &gt; &le; &ge;".Split(' ');
         public static readonly string[] JavaCommentLiterals = "// /*  */".Split(' ');
